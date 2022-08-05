055023c326a9cad8425128fba0ce3571899d68ab
Praba <prama1303@gmail.com>
2022-08-06 00:46:06 +0200

    views site for routes added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', require('./routes/root'));
+
+//404 pages
+app.all('*', (req, res) => {
+  //res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
